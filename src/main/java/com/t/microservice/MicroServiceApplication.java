7562226b37b0d021fe6a2998462c9885675375c3
syrongk <syrongk@gmail.com>
2022-09-15 22:24:25 +0900

    basic environment

--- src/main/java/com/t/microservice/MicroServiceApplication.java
+++ src/main/java/com/t/microservice/MicroServiceApplication.java
@@ -2,8 +2,9 @@ package com.t.microservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class MicroServiceApplication {
 
     public static void main(String[] args) {
